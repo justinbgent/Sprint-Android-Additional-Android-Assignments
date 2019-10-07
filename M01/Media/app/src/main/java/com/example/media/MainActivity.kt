48b892af6705f5d8c832f103a08c6b1dfd33a955
Justin Gent <justinbgent@gmail.com>
2019-10-07 15:36:16 -0600

    Seekbar updates on progress every second and responds to user changes.

--- M01/Media/app/src/main/java/com/example/media/MainActivity.kt
+++ M01/Media/app/src/main/java/com/example/media/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.media
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.media.MediaPlayer
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +30,48 @@ class MainActivity : AppCompatActivity() {
             video_view.pause()
         }
 
+        val handler = Handler()
+        runOnUiThread(object : Runnable{
+            override fun run() {
+                if (mediaPlayer != null){
+                    seek_bar.progress = (mediaPlayer as MediaPlayer).currentPosition
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
 
         seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                if (p0 != null){
-                    video_view.seekTo(p1)
+            override fun onProgressChanged(seekBar: SeekBar?, p1: Int, p2: Boolean) {}
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null){
+                    video_view.seekTo(seekBar.progress)
                 }
             }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        video_view.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.img_9724))
+
+
+        video_view.setOnPreparedListener {
+            mediaPlayer = it
+            play_btn.isEnabled = true
+            pause_btn.isEnabled = true
+            if (it != null){
+                seek_bar.max = it.duration
+            }
+        }
+
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        video_view.pause()
     }
 }
