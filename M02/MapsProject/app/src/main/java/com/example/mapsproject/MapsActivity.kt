42b59f5ac70b3aedccf9257a46bb816580837c1a
Justin Gent <justinbgent@gmail.com>
2019-10-08 15:41:28 -0600

    Add functions to move map to user location and add pin there.

--- M02/MapsProject/app/src/main/java/com/example/mapsproject/MapsActivity.kt
+++ M02/MapsProject/app/src/main/java/com/example/mapsproject/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mapsproject
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,6 +15,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -39,5 +46,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        position_btn.setOnClickListener {
+            requestPermission()
+
+            //val latLng = mMap.cameraPosition.target
+
+        }
+    }
+
+    fun requestPermission(){
+        ActivityCompat
+            .requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                232)
+    }
+
+    fun getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+
+            val locationProvider = LocationServices.getFusedLocationProviderClient(this)
+            //val location: Location? = locationProvider.lastLocation.result
+
+            locationProvider.lastLocation.addOnSuccessListener {
+                if (it != null) {
+                    val latLng = LatLng(it.latitude, it.longitude)
+                    mMap.addMarker(MarkerOptions().position(latLng).title("Your Location"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                }
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 232) {
+            getCurrentLocation()
+        }
     }
 }
